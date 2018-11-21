bfe6c32f1c01666652b0e1d695886a624ae03745
Ilya Sychev <ILYA.SYCHEV@raiffeisen.ru>
2018-11-21 15:51:00 +0300

    init project

---
+++ blockchain/BoolTransfer.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.22;
+
+contract BoolTransfer
+{
+	public bool superData;
+	
+	constructor() public
+	{
+		superData = true;
+	}
+}
---
+++ frontend/src/abi.js
@@ -0,0 +1,24 @@
+const abi = [
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "superData",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  }
+];
+
+export default abi;
